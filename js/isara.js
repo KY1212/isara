0ca33d677a75bd99da34a0a71e859e18a1e10212
KY1212 <YumYum1212@docomo.ne.jp>
2020-07-22 18:25:41 +0900

    add smoSmooth scroll

--- js/isara.js
+++ js/isara.js
@@ -9,4 +9,23 @@ $(function () {
     $(this).find(contents).stop(true).slideToggle();
   });
 
+  $(function(){
+    var pagetop = $('#pageTop');
+    // ボタン非表示
+    pagetop.hide();
+  
+    // 100px スクロールしたらボタン表示
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+            pagetop.fadeIn();
+          } else {
+            pagetop.fadeOut();
+          }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500);
+      return false;
+    });
+  });
+
 });
