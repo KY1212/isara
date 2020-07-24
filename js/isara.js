0b79476bdfb6f3d5460a7e5fbc8b98ecd05169cd
KY1212 <YumYum1212@docomo.ne.jp>
2020-07-24 12:47:17 +0900

    update ホバー時色の変更、ドロップダウンのアイコン修正

--- js/isara.js
+++ js/isara.js
@@ -3,10 +3,27 @@ $(function () {
   const box = $(".boxesSlide .box");
   const heading = $(".box .heading");
   const contents = $(".box .contents");
+  const icon = $(".boxesSlide .fas");
+  let rotateIcon = 180;
+
 
   box.click(function(){
+
 		$(heading).not(this).next(contents).slideUp();
     $(this).find(contents).stop(true).slideToggle();
+
+    $(this).find(icon).css("transform",`rotate(${rotateIcon}deg)`),
+    $(this).find(icon).css("transition","transform 0.6s");
+
+    console.log("終"+rotateIcon);
+
+    if(rotateIcon==0){
+      rotateIcon= rotateIcon+180;
+      console.log("0 "+rotateIcon);
+    }else if(rotateIcon>=180){
+      rotateIcon=0;
+      console.log("180 "+rotateIcon);
+    }
   });
 
   $(function(){
